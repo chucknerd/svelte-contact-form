277fe3bb4f9ebc668b6765f608458bfb0c099de5
chucknerd <chucknerd1@gmail.com>
2020-02-18 04:32:40 -0600

    First Commit

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import ContactList from ".components/ContactList.svelte";
+  import ContactForm from ".components/ContactForm.svelte";
+</script>
+
+<main>
+  <h1>State managment!</h1>
+  <ContactForm />
+  <ContactList />
+</main>
---
+++ src/ContactList.svelte
@@ -0,0 +1,11 @@
+<script>
+  import contacts from "./src/contacts";
+</script>
+
+<div id="contactList">
+  {#each $contacts as contact}
+    <p>{contact.name} {contact.surname}</p>
+    <p>{contact.mail}</p>
+    <hr />
+  {/each}
+</div>
